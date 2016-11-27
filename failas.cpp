d07add9c5a24088d0d9bd7e29cb2ace540fa3f3a
Nastaravicius <lukas.nastaravicius@gmail.com>
2016-11-27 14:30:47 +0200

    Update failas.cpp

--- failas.cpp
+++ failas.cpp
@@ -4,7 +4,18 @@ using namespace std;
 
 int main() {
   
+  int amzius;
+  
   cout << "Sveiki!" << endl;
 
+  cout << "Iveskite savo amziu: ";
+  cin >> amzius;
+  
+  if(amzius >= 18) {
+      cout << "Jus atitinkate reikalavimus!" << endl; 
+  } else {
+      cout << "Jusu amzius per mazas, kad atitiktu reikalavimus! " << endl;
+  }
+  
   return 0;
 }
