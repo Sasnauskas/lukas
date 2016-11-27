106e1126a01318ec3647eba4317454ec74b6d764
Sasnauskas <jingrindan@gmail.com>
2016-11-27 14:27:51 +0200

    Update failas.cpp

--- failas.cpp
+++ failas.cpp
@@ -3,19 +3,8 @@
 using namespace std;
 
 int main() {
-
-  int amzius;
   
   cout << "Sveiki!" << endl;
-  
-  cout << "Iveskite savo amziu: ";
-  cin >> amzius;
-  
-  if(amzius >= 18) {
-      cout << "Jus tinkamas siam projektui" << endl; 
-  } else {
-    cout << "Jusu amzius neatitinka reikalavimu" << endl;
-  }
 
   return 0;
 }
